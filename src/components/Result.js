915579ad9df193a327bd8d2306d3131507cba89b
Jan Kohlbach <iamjkdesign@gmail.com>
2019-05-14 22:03:13 +0200

    add result text dependent on score

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,10 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Result(props) {
+  const percentage = Math.round(props.resultPoints / props.resultMaxPoints * 100);
+  let resultText = '';
+
+  switch(true) {
+    case percentage < 20:
+      resultText = '0% - 19%';
+      break;
+
+    case percentage < 40:
+      resultText = '20% - 39%';
+      break;
+
+    case percentage < 60:
+      resultText = '40% - 59%';
+      break;
+
+    case percentage < 80:
+      resultText = '60% - 79%';
+      break;
+
+    case percentage <= 100:
+      resultText = '80% - 100%';
+      break;
+
+    default:
+      break;
+  }
+
   return (
     <div className="result">
       <h2>{props.resultPoints} of {props.resultMaxPoints} points</h2>
-      <h2>{Math.round(props.resultPoints / props.resultMaxPoints * 100)}%</h2>
+      <h2>{percentage}%</h2>
+      <p>{resultText}</p>
     </div>
   )
 }
